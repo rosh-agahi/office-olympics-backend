99f54af98d78eb247c2eef406abc8697acb222b1
Roshanak <rosh.agahi@gmail.com>
2021-08-07 13:30:05 -0700

    challenges create

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -2,10 +2,17 @@ class Api::V1::ChallengesController < ApplicationController
 
   def index
     @challenges = Challenge.all
-    render json: @challenges 
+    render json: @challenges
   end
 
   def create
+    @challenge = Challenge.new(challenge_params)
+    if @challenge.save
+      render json: @challenge 
+    else
+      render json: {error: 'Error saving the challenge'}
+    end
+
   end
 
   def show
