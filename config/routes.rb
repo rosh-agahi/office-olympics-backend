62a6dbee473074c30a4560f775f2a93c0ea047d1
Roshanak <rosh.agahi@gmail.com>
2021-08-07 12:49:28 -0700

    namespace api v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :submissions
-  resources :challenges
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :submissions
+      resources :challenges
+    end 
+  end
 end
